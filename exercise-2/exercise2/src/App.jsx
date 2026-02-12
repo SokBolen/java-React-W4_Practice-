401ad4050f3a50dd9607c96be515b31119cf95a9
Noy Sokbolen <bolen0096@gmail.com>
2026-02-12 16:04:35 +0700

    work

---
+++ exercise-2/exercise2/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Header from './Header'
+import Main from './Mains'
+import Footer from './Footer'
+
+
+function App() {
+  
+  return (
+    <>
+      <p>Hello</p>
+      < Header/>
+      < Main/>
+      < Footer/>   
+    </>
+  );
+}
+
+export default App
